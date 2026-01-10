dd7e3a0b2fa38ef9f5949ba16f669f1de3e906fb
Jack Green <jack_s_green@me.com>
2026-01-10 18:23:49 +0200

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>
 );
