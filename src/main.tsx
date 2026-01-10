1bbcb036ec856f9dcd14cd1e980458dc50860fe8
Jack Green <jack_s_green@me.com>
2026-01-10 18:33:43 +0200

    posthog

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-11-30",
+} as const;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <SpeedInsights />
-    <Analytics />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+      <SpeedInsights />
+      <Analytics />
+    </PostHogProvider>
   </StrictMode>
 );
